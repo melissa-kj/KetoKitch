1c7029d52cc33c6c2e7b04e645fcb0aa52bed153
Melissa Jeremie <melissajeremie@gmail.com>
2019-11-13 16:51:03 -0500

    refactoring front end to use axios instead of fetch

--- Client-FrontEnd/ketokitch/src/App.js
+++ Client-FrontEnd/ketokitch/src/App.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+
 
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      loggedIn: false,
+      user: {}
+    }
+  }
   render() {
     return (
       <div className="app">
         <Router>
           <Switch>
-            <Route exact path={"/"} component={Home} />
+            <Route
+            exact
+            path={"/"}
+            render={props => (
+              <Home {...props} loggedIn={this.state.loggedIn} />
+            )}
+             />
+            <Route path={"/dashboard"} component={Dashboard} />
           </Switch>
         </Router>
       </div>
