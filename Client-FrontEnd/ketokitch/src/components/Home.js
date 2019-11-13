578e700bb0795e9cc55a170b63573d74b244555d
Melissa Jeremie <melissajeremie@gmail.com>
2019-11-13 14:08:15 -0500

    file structure change

--- Client-FrontEnd/ketokitch/src/components/Home.js
+++ Client-FrontEnd/ketokitch/src/components/Home.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import Signup from "./Signup";
+import Login from "./Login";
 
 export default class Home extends Component {
 
   render() {
     return (
       <div>
-        <h1>keto kitch home </h1>
-        <h1>keto kitch home </h1>
+        <h1>keto kitch</h1>
+        <h1> home </h1>
         <Signup />
+        <Login />
       </div>
     );
   }
---
+++ Client-FrontEnd/ketokitch/src/components/Login.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+// import Home from './components/Home';
+
+
+export default class Login extends Component {
+constructor(props) {
+  super(props);
+
+  this.state = {
+    username: "",
+    password: "",
+    loggedIn: false
+  }
+
+  this.handleSubmit = this.handleSubmit.bind(this);
+  this.handleChange = this.handleChange.bind(this);
+}
+handleChange(event) {
+  this.setState({
+    [event.target.name]: event.target.value
+  });
+  console.log("handle change", event);
+}
+handleSubmit(event) {
+  const {
+    username,
+    password
+  } = this.state;
+
+  axios.post("http://localhost:8080/login", {
+      username: username,
+      password: password,
+      loggedIn: true
+  }).then(response => {
+    localStorage.setItem('user', response.token);
+    console.log("login response", response);
+  })
+  .catch(error => {
+    console.log("login error", error);
+  })
+  console.log("login successful");
+  event.preventDefault();
+}
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.handleChange}
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              required
+            />
+
+          <button type="submit">Login</button>
+
+        </form>
+      </div>
+    );
+  }
+}
+// .then(response => {
+// createRecipe();
+// })
+// .then(response => {
+// createComment();
+// })
+// .then(response =>{
+// deleteRecipe();
+// })
+// .then(response =>{
+// deleteComment();
+// })
+// .then(response =>{
+// profile();
+// })
