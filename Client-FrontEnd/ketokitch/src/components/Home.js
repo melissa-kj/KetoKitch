516325035a761f725dc1144199e502e405e4503e
Melissa Jeremie <melissajeremie@gmail.com>
2019-11-14 01:12:52 -0500

    initializig state for login and sign up

--- Client-FrontEnd/ketokitch/src/components/Home.js
+++ Client-FrontEnd/ketokitch/src/components/Home.js
@@ -3,6 +3,7 @@ import Signup from "./Signup";
 import Login from "./Login";
 
 export default class Home extends Component {
+  // attempting to set state for both login and signup here 
   constructor(props) {
     super(props);
     this.state = {
@@ -14,7 +15,35 @@ export default class Home extends Component {
       loggedIn: false
     }
   }
+  LogOn = (e) => {
+    e.preventDefault();
 
+    fetch("http://localhost:8080/login", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify ({
+        email: this.state.user.email,
+        username: this.state.user.username,
+        password: this.state.user.password
+      })
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      console.log(res, "Token successful");
+      this.setState({
+        user: {... this.state.user},
+        loggedIn: true,
+      })
+      localStorage.setItem('user', res.token);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
   render() {
     return (
       <div>
